590e76c463afa6fefba9bd0812005ddea8557149
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2022-11-05 12:31:06 +0700

    main: Complete task 17.6.2

--- 17.6/17_6.js
+++ 17.6/17_6.js
@@ -1,4 +1,13 @@
-function sum(a, b) {
-    return a - b;
+function getPercents(percent, number) {
+    if(percent > 0) {
+      return number / 100 * percent;
+    } 
+    else if (percent === 0) {
+      return number
+    }
+    else {
+      return "Wrong Data"
+    }
+    
   }
-module.exports = sum;
+module.exports = getPercents;
--- 17.6/17_6.test.js
+++ 17.6/17_6.test.js
@@ -1,5 +1,16 @@
-const sum = require('./17_6');
+const getPercents = require('./17_6');
 
-test('adds 1 - 2 to equal -1', () => {
-  expect(sum(1, 2)).toBe(-1);
+describe('tests for getPersents funciton', () => {
+  const randomNum = Math.random()
+  it('71 of 1059 equal to 751.89', () => {
+    expect(getPercents(71, 1059)).toBe(751.89)
+  })
+
+  it('-1 percets of random number equal to "Wrong Data"', () => {
+    expect(getPercents(-1, randomNum)).toBe('Wrong Data')
+  })
+  
+  it('0 percets of random number equal to random number', () => {
+    expect(getPercents(0, randomNum)).toBe(randomNum)
+  })
 });
